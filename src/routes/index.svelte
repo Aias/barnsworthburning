c5b8b08bf0b05488cc1769b63a19863bb203a83e
Nick Trombley <trombley.nick@gmail.com>
2019-07-23 16:48:57 -0400

    Style stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,22 @@
+<script>
+	import AstrophytonVerrucosum from '../components/vectors/AstrophytonVerrucosum.svelte';
+</script>
+
 <svelte:head>
 	<title>barnsworthburning.net</title>
 </svelte:head>
 
-<img src="images/negativity.jpg" alt="Negativity" />
+<blockquote cite="https://www.bartleby.com/198/1.html">
+	I should have been a pair of ragged claws <br />
+	Scuttling across the floors of silent seas.
+</blockquote>
+
+<AstrophytonVerrucosum />
 
 <style>
-	img {
-		max-width: 100%;
+	blockquote {
+		font-style: italic;
+		margin: 0;
+		text-align: center;
 	}
 </style>
