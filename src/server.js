e6fe070ca3d1680576d265149dce21544cf2294b
Nick Trombley <trombley.nick@gmail.com>
2019-07-23 08:59:26 -0400

    Attempt to fix API issues

--- src/server.js
+++ src/server.js
@@ -17,6 +17,15 @@ const devProxy = {
 	}
 };
 
+// Set up API proxy.
+server.use(
+	proxyMiddleware('/api', {
+		target: 'https://barnsworthburning-api.netlify.com/.netlify/functions',
+		pathRewrite: { '^/api': '' },
+		changeOrigin: true
+	})
+);
+
 // Set up the proxy.
 if (dev && devProxy) {
 	Object.keys(devProxy).forEach(function(context) {
