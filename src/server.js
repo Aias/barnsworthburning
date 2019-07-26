8a4843da66c82bb10365d6c090d25c894d13ec79
Nick Trombley <trombley.nick@gmail.com>
2019-07-25 22:19:23 -0400

    Remove unnecessary dependencies, tweak dark theme

--- src/server.js
+++ src/server.js
@@ -2,21 +2,11 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-// import proxyMiddleware from 'http-proxy-middleware';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 const server = polka();
-
-// Set up API proxy.
-// server.use(
-// 	proxyMiddleware('/api', {
-// 		target: 'https://barnsworthburning-api.netlify.com/.netlify/functions',
-// 		pathRewrite: { '^/api': '' },
-// 		changeOrigin: true
-// 	})
-// );
 
 server // You can also use Express
 	.use(
