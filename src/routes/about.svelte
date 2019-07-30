fd83c476641ad0413cea9f1223b54480f7a2e013
Nick Trombley <trombley.nick@gmail.com>
2019-07-30 15:29:10 -0400

    Update dependencies

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<!--
+	A reminder to myself to include at least a passing reference to *the real Nick Trombley*, method actor, and almost certainly the person whose site you're actually looking for.
+
+	https://www.nicktrombley.com/
+-->
