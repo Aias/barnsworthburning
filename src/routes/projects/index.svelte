cefd8613c950e48f7a57e8283c659c4fa9978e6f
Nick Trombley <trombley.nick@gmail.com>
2019-08-09 23:45:41 -0400

    Minor cleanup to global styles and projects list

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,11 +1,11 @@
-<nav>
-	<h1>Things</h1>
-	<ul>
-		<li>
-			<a href="projects/fitter">Fitter</a>
-		</li>
-		<li>
-			<a href="projects/patterns">Pattern Grid</a>
-		</li>
-	</ul>
-</nav>
+<script>
+	import Link from '../../components/Link.svelte';
+</script>
+
+<p class="text-mono">
+	Check back sometime. I'm sure there will be things here eventually. Until
+	then, <Link href="https://github.com/Aias">Github</Link> is a pretty good indication of what I've been working on. If you insist on staying here and don't mind overheating your computer, you can check out this <Link href="/projects/patterns">patterns grid</Link>, which summarizes all of the interrelationships of all 253 patterns from Christopher Alexander's seminal work.
+</p>
+<p class="text-mono">
+	That's all I've got for now, I swear.
+</p>
