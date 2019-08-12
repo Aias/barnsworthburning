02e4bcba9006b89545796b4612b68ad97fc3b2eb
Nick Trombley <trombley.nick@gmail.com>
2019-08-12 10:14:19 -0400

    Fix mode initialization

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-let hourOfDay = new Date().getHours;
+let hourOfDay = new Date().getHours();
 let isSleepingTime = hourOfDay <= 6 || hourOfDay >= 20;
 
 const isDarkMode = writable(isSleepingTime);
