9040a45592bd87aacfd387fe3240a6f5edb43b3e
Nick Trombley <trombley.nick@gmail.com>
2019-08-20 16:50:10 -0500

    Fix flash of opposite mode bug

--- src/stores/mode.js
+++ src/stores/mode.js
@@ -3,6 +3,6 @@ import { writable } from 'svelte/store';
 let hourOfDay = new Date().getHours();
 let isSleepingTime = hourOfDay <= 6 || hourOfDay >= 20;
 
-let isDarkMode = writable(isSleepingTime);
+let isDarkMode = writable(false); // Always start in day theme until I can figure out how to prevent flash of opposite mode.
 
 export { isDarkMode };
