21cf6b9d7a42210d8f59a39bf6bf2d6e8e3b4a8b
Nick Trombley <ntrombley@genospace.com>
2020-08-19 18:00:32 -0400

    start transition to new database

---
+++ src/components/Index.svelte
@@ -0,0 +1,83 @@
+<script>
+	export let creators = [];
+	export let spaces = [];
+
+	$: index = creators
+		.map((c) => ({ ...c, type: 'creator' }))
+		.concat(spaces.map((s) => ({ ...s, type: 'space' })))
+		.sort(compareFields);
+
+	const getSortField = (obj) => {
+		if (obj.type === 'creator') {
+			return obj.last_name || '';
+		} else {
+			return obj.topic || '';
+		}
+	};
+
+	const compareFields = (a, b) => {
+		return getSortField(a).toLowerCase() > getSortField(b).toLowerCase()
+			? 1
+			: getSortField(a).toLowerCase() < getSortField(b).toLowerCase()
+			? -1
+			: 0;
+	};
+
+	const lastFirst = (creator) => {
+		const last = creator.last_name;
+		const full = creator.full_name;
+
+		if (last === full) {
+			// e.g., single-word names like Homer or Virgil
+			return full;
+		}
+
+		const lastLoc = full.indexOf(last);
+		const firstPart = full.substring(0, lastLoc).trim();
+
+		return `${last}, ${firstPart}`;
+	};
+</script>
+
+<ol>
+	{#each index as {type, ...node}, i}
+	<li>
+		{#if type === 'creator'}
+		<a href="#creator/{node.slug}">{lastFirst(node)}</a>&nbsp;<span class="text-secondary">
+			{node.extracts ? node.extracts.length : 0}
+		</span>
+		{:else}
+		<a href="#space/{node.topic}">{node.topic}</a>&nbsp;<span class="text-secondary">
+			{node.extracts ? node.extracts.length : 0}
+		</span>
+		{/if}
+	</li>
+	{/each}
+</ol>
+
+<style>
+	ol {
+		list-style-type: none;
+		column-width: 20ch;
+		column-gap: 5ch;
+		margin: 1rem;
+		font-size: 10.5px;
+	}
+
+	li {
+		--indent: 1em;
+		padding-left: var(--indent);
+		text-indent: calc(-1 * var(--indent));
+	}
+
+	span {
+		margin-left: 1em;
+		text-align: right;
+		opacity: 0.25;
+		transition: opacity 0.15s;
+	}
+
+	li:hover > span {
+		opacity: 1;
+	}
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,100 +1,51 @@
+<script context="module">
+	import select from '../helpers/select.js';
+
+	export async function preload(page, session) {
+		const creators = await select('creators', {
+			fields: ['full_name', 'last_name', 'extracts', 'spaces', 'last_updated', 'slug'],
+			filterByFormula: `{num_extracts} > 0`
+		})(this.fetch);
+
+		const spaces = await select('spaces', {
+			fields: ['topic', 'extracts', 'creators', 'last_updated'],
+			filterByFormula: `{num_extracts} > 1`
+		})(this.fetch);
+
+		return { creators, spaces };
+	}
+</script>
+
 <script>
 	import { stores } from '@sapper/app';
 	import { derived } from 'svelte/store';
 	import { isDarkMode, loading } from '../stores';
 	import getEmojiForTheme from '../helpers/getEmojiForTheme';
 
-	import Header from '../components/Header.svelte';
-	import Loading from '../components/Loading.svelte';
 	import SEO from '../components/SEO.svelte';
+	import Index from '../components/Index.svelte';
 
 	let { preloading } = stores();
 	let loadingany = derived([preloading, loading], ([$preloading, $loading]) => $preloading || $loading);
 
 	export let segment;
+	export let creators;
+	export let spaces;
 </script>
 
 <SEO />
 
 <div class="{getEmojiForTheme($isDarkMode)}">
 	<header>
-		<Header {segment} />
+		<nav>
+			<Index {creators} {spaces} />
+		</nav>
 	</header>
 	<main>
 		<slot></slot>
 	</main>
-	{#if $loadingany}
-	<Loading />
-	{/if}
 </div>
 
 <style>
-	div {
-		position: fixed;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		height: 100%;
-		overflow: hidden;
-		padding: 3rem 2rem 2rem 3rem;
-		
-		display: grid;
-		grid-template-columns: [header-start toolbar-start loading-start] auto [header-end loading-end main-start footer-start] 1fr [main-end toolbar-end footer-end];
-		grid-template-rows: [toolbar-start] auto [toolbar-end header-start loading-start main-start] 1fr [header-end loading-end footer-start  main-end] auto [footer-end];
-
-		color: var(--text-primary);
-		background-color: var(--layer-bg);
-
-		transition: color 0.5s, background-color 0.5s;
-	}
-
-	header, main {
-		position: relative;
-	}
-
-	header {
-		grid-area: header;
-		overflow-y: hidden;
-	}
-
-	main {
-		grid-area: main;
-		overflow-y: auto;
-		padding: 0 1rem 0 1rem;
-		margin-right: -1rem;
-		scrollbar-color: var(--clr-scrollbar) transparent;
-		scrollbar-width: thin;
-	}
-
-	@media(max-width: 750px) {
-		div {
-			position: relative;
-			height: auto;
-			min-height: 100vh;
-			overflow: initial;
-			grid-template-columns: [header-start loading-start main-start footer-start] 1fr [toolbar-start] auto [header-end toolbar-end main-end loading-end footer-end];
-			grid-template-rows: [toolbar-start header-start loading-start] auto [header-end toolbar-end loading-end main-start] 1fr [main-end footer-start] auto [footer-end];
-			padding: 1rem 0.5rem;
-		}
-
-		header {
-			padding-bottom: 0.5rem;
-			padding-left: 1rem;
-		}
-
-		header:after {
-			content: '⁘';
-			display: block;
-			text-align: center;
-			padding-top: 0.5rem;
-			padding-right: 1rem;
-			color: var(--text-secondary);
-		}
-
-		header, main {
-			margin-left: 0;
-			margin-right: 0;
-		}
-	}
+	
 </style>
