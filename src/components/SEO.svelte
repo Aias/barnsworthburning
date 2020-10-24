a39979dd69ad821a4b441b5b080302eb2101ea4e
Nick Trombley <ntrombley@genospace.com>
2020-10-24 13:20:38 -0400

    fix duplicate head tags

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -4,10 +4,9 @@
 
 <svelte:head>
 	<title>{$seo.title}</title>
-	<meta property="og:title" content="{$seo.title}" />
+	<meta property="og:type" content="website" />
 	<meta name="author" content="{$seo.author}" />
 	<meta name="description" content="{$seo.description}" />
-	<meta property="og:description" content="{$seo.description}" />
 	<meta name="keywords" content="{$seo.keywords}" />
 	<meta name="theme-color" content="{$seo.themeColor}" />
 	<meta name="monetization" content="$ilp.uphold.com/AqdQQ2khBpyN" />
