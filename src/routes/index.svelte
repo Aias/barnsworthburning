1568aa77625680388e31ff9f800aecbde81dbb91
Nick Trombley <ntrombley@genospace.com>
2021-02-27 14:23:46 -0500

    add programmatic navigation to deep routes

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { goto } from '@sapper/app';
+	import { onMount } from 'svelte';
+
+	// The following is a fix for making sure we navigate directly to deeply nested routes.
+	onMount(() => {
+		console.log('Navigating');
+		console.log(window.location);
+		if (window.location.pathname !== '/') {
+			goto(window.location.pathname);
+		}
+	});
+</script>
