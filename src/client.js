3f02b1a4c7d71634ec6c287a6fad003ff7efbf3c
Nick Trombley <trombley.nick@gmail.com>
2021-04-09 06:02:04 -0400

    migration up to 'Renamed Files'

--- src/client.js
+++
@@ -1,5 +0,0 @@
-import * as sapper from '@sapper/app';
-
-sapper.start({
-	target: document.querySelector('#🕹️')
-});
---
+++ svelte.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+	// options passed to svelte.compile (https://svelte.dev/docs#svelte_compile)
+	compilerOptions: null,
+
+	// an array of file extensions that should be treated as Svelte components
+	extensions: ['.svelte'],
+
+	kit: {
+		adapter: null,
+		amp: false,
+		appDir: '_app',
+		files: {
+			assets: 'static',
+			hooks: 'src/hooks',
+			lib: 'src/lib',
+			routes: 'src/routes',
+			// serviceWorker: 'src/service-worker',
+			template: 'src/app.html'
+		},
+		host: null,
+		hostHeader: null,
+		hydrate: true,
+		paths: {
+			assets: '',
+			base: ''
+		},
+		prerender: {
+			crawl: true,
+			enabled: false,
+			force: false,
+			pages: ['*']
+		},
+		router: true,
+		ssr: true,
+		target: null,
+		vite: () => ({})
+	},
+
+	// options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
+	preprocess: null
+};
