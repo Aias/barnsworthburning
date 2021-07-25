74215c6d7dc62b6d7f0d859832f29208be355426
Nick Trombley <trombley.nick@gmail.com>
2021-07-25 12:49:59 -0400

    increase feed lookback period (2x per day)

--- src/routes/feed.xml.js
+++ src/routes/feed.xml.js
@@ -157,7 +157,7 @@ export async function get() {
 			'extract_image',
 			'image_caption'
 		],
-		filterByFormula: `last_updated < DATEADD(NOW(), -2, 'hour')`
+		filterByFormula: `last_updated < DATEADD(NOW(), -12, 'hour')`
 	};
 
 	const works = await airtableFetch('extracts', workOptions);
