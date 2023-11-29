de2d30dffebb218daa43b35fe387258ebda0e906
Nick Trombley <trombley.nick@gmail.com>
2023-11-28 21:35:05 -0500

    Much simpler extract ordering function

--- src/routes/(app)/[extractId]/+page.svelte
+++ src/routes/(app)/[extractId]/+page.svelte
@@ -5,53 +5,26 @@
 
 	$: extracts = data.extracts || [];
 	$: currentId = data.currentId;
-	let parentExtract,
-		childExtracts = [];
 
-	const makeChildSortFunction =
-		(childOrder = []) =>
-		(a, b) => {
-			const indexA = childOrder.findIndex((val) => val.id === a.id);
-			const indexB = childOrder.findIndex((val) => val.id === b.id);
-
-			if (indexA > indexB) return 1;
-			else return -1;
-		};
-
-	const getChildren = (extracts, parentIndex) => {
-		if (!parentExtract.children) return undefined;
-
-		let children = [...extracts];
-		children.splice(parentIndex, 1);
-		const childOrder = parentExtract.children || [];
-
-		return children.sort(makeChildSortFunction(childOrder));
-	};
-
-	$: {
-		if (extracts) {
-			let parentIndex = extracts.findIndex((e) => {
-				return e.id === currentId;
-			});
-			if (parentIndex === -1) parentIndex = 0;
-
-			parentExtract = extracts[parentIndex];
-			childExtracts = getChildren(extracts, parentIndex);
-		}
-	}
+	$: parentExtract = extracts.find((e) => e.id === currentId);
+	$: childExtracts = parentExtract?.children
+		? parentExtract.children.map((c) => extracts.find((e) => e.id === c.id))
+		: null;
 </script>
 
-<header class="card">
-	<Extract extract={parentExtract} />
-</header>
-{#if childExtracts}
-	<ol>
-		{#each childExtracts as child (child.id)}
-			<li>
-				<Extract extract={child} />
-			</li>
-		{/each}
-	</ol>
+{#if parentExtract}
+	<header class="card">
+		<Extract extract={parentExtract} />
+	</header>
+	{#if childExtracts}
+		<ol>
+			{#each childExtracts as child (child.id)}
+				<li>
+					<Extract extract={child} />
+				</li>
+			{/each}
+		</ol>
+	{/if}
 {/if}
 
 <style>
