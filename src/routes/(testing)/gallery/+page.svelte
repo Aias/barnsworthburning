de0188b0a00129b9cbc368bd77a34c8216ebd148
Nick Trombley <trombley.nick@gmail.com>
2023-12-06 15:41:33 -0700

    Somehow this works?

---
+++ src/routes/(testing)/gallery/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import '$styles/app.css';
+	import { page } from '$app/stores';
+
+	import Component from './Component.svelte';
+
+	let searchParams = $derived($page.url.searchParams);
+	let creator = $derived(searchParams.get('creator'));
+	let space = $derived(searchParams.get('space'));
+
+	let gallery = $state([]);
+
+	$effect(async () => {
+		if (creator) {
+			const data = await getGallery('creator', creator);
+			gallery = [...data.gallery];
+		} else if (space) {
+			const data = await getGallery('space', space);
+			gallery = [...data.gallery];
+		}
+	});
+
+	async function getGallery(entityType, entityId) {
+		const response = await fetch(`/api/gallery/${entityType}/${entityId}`)
+			.then((r) => r.json())
+			.catch(console.error);
+		if (response) {
+			return response;
+		}
+	}
+</script>
+
+<ul>
+	<li><a href="?creator=recaR62tQy0mFgzgE">Christopher Alexander</a></li>
+	<li><a href="?creator=recUl87Cd65bPFBcF">Alan Jacobs</a></li>
+	<li><a href="?creator=rec97tRUYZBhAs6rZ">Nick Trombley</a></li>
+</ul>
+
+{#if creator && gallery.length > 0}
+	<Component extract={gallery[0]} />
+{:else}
+	<Component />
+{/if}
---
+++ src/routes/(testing)/gallery/Component.svelte
@@ -0,0 +1,14 @@
+<script>
+	let { extract } = $props();
+
+	extract;
+	// let title = $derived(extract?.title);
+	// let title = extract?.title;
+	let derived = $derived(extract?.title);
+	let direct = extract?.title;
+</script>
+
+<div>
+	<h2>Direct: {direct}</h2>
+	<h2>Derived: {derived}</h2>
+</div>
