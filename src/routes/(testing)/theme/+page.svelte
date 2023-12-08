d980a98d0c1fbd6e9035852c00206d9533bd08ff
Nick Trombley <trombley.nick@gmail.com>
2023-12-07 21:36:11 -0500

    Fix theme testbench

--- src/routes/(testing)/theme/+page.svelte
+++ src/routes/(testing)/theme/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '$styles/app.css';
+	import TestCard from './TestCard.svelte';
 
 	const themes = [
 		{},
@@ -15,26 +16,8 @@
 		{ chroma: 'neutral', mode: 'dark' },
 		{ chroma: 'neutral', palette: 'green' }
 	];
-	const testSections = [
-		'light',
-		'dark',
-		'light chromatic',
-		'dark chromatic'
-	];
+	const testSections = ['light', 'dark', 'light chromatic', 'dark chromatic'];
 </script>
-
-{#snippet testCard({ mode, palette, chroma })}
-<article class={`${mode || ''} ${palette || ''} ${chroma || ''}`}>
-	<h3>Attention to Detail</h3>
-	<p>Truly functional design only comes from the most careful and intense attention to detail.</p>
-	<a href="./">Christopher Alexander</a>
-	<ul>
-		{#if mode}<li>#{mode}</li>{/if}
-		{#if palette}<li>#{palette}</li>{/if}
-		{#if chroma}<li>#{chroma}</li>{/if}
-	</ul>
-</article>
-{/snippet}
 
 <main>
 	{#each testSections as testClass}
@@ -42,21 +25,23 @@
 			<h2>{testClass}</h2>
 			<div>
 				{#each themes as { mode, palette, chroma }}
-					{@render testCard({ mode, palette, chroma })}
-				{/each}		
+					<TestCard {mode} {palette} {chroma} />
+				{/each}
 			</div>
 		</section>
-	{/each}	
+	{/each}
 </main>
 
-
 <style lang="scss">
+	h2 {
+		text-transform: capitalize;
+	}
 	section {
 		padding: 1rem;
 		background-color: var(--background);
 		display: flex;
 		flex-direction: column;
-		gap: 1rem;	
+		gap: 1rem;
 	}
 	div {
 		--gap: 1rem;
@@ -68,37 +53,5 @@
 
 	div > :global(*) {
 		flex: 0 0 calc(100% / 3 - var(--gap));
-	}
-
-	article {
-		padding: 1rem;
-		border: 1px solid var(--divider);
-		border-radius: 4px;
-		background-color: var(--paper);
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-		&:hover {
-			border-color: var(--border);
-			background-color: var(--container);
-			transition: all 200ms;
-		}
-	}
-
-	h2, h3 {
-		text-transform: capitalize;
-	}
-	p {
-		color: var(--display);
-	}
-	ul {
-		display: flex;
-		flex-direction: row;
-		gap: 0.5rem;
-		list-style: none;
-		margin: 0;
-		padding: 0;
-		color: var(--hint);
-		font-size: 0.75rem;
 	}
 </style>
---
+++ src/routes/(testing)/theme/TestCard.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let mode;
+	export let palette;
+	export let chroma;
+</script>
+
+<article class={`${mode || ''} ${palette || ''} ${chroma || ''}`}>
+	<h3>Attention to Detail</h3>
+	<p>Truly functional design only comes from the most careful and intense attention to detail.</p>
+	<a href="./">Christopher Alexander</a>
+	<ul>
+		{#if mode}<li>#{mode}</li>{/if}
+		{#if palette}<li>#{palette}</li>{/if}
+		{#if chroma}<li>#{chroma}</li>{/if}
+	</ul>
+</article>
+
+<style lang="scss">
+	article {
+		padding: 1rem;
+		border: 1px solid var(--divider);
+		border-radius: 4px;
+		background-color: var(--paper);
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		&:hover {
+			border-color: var(--border);
+			background-color: var(--container);
+			transition: all 200ms;
+		}
+	}
+
+	h3 {
+		text-transform: capitalize;
+	}
+	p {
+		color: var(--display);
+	}
+	ul {
+		display: flex;
+		flex-direction: row;
+		gap: 0.5rem;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		color: var(--hint);
+		font-size: 0.75rem;
+	}
+</style>
