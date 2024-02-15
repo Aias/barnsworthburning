abd6d6044790ab696b36b636c39b20d821485bd9
Nick Trombley <trombley.nick@gmail.com>
2024-02-15 12:38:14 -0500

    Remove comments

--- src/helpers/zip.ts
+++ src/helpers/zip.ts
@@ -1,24 +1,3 @@
-// export default function zip(keys = [], ...arrays) {
-// 	const length = arrays[0]?.length;
-// 	if (!length) return undefined;
-// 	const zippedArray = [];
-
-// 	for (let i = 0; i < length; i++) {
-// 		const obj = {};
-
-// 		for (let j = 0; j < arrays.length; j++) {
-// 			const key = keys[j] || `array${j + 1}`;
-// 			obj[key] = arrays[j][i];
-// 		}
-
-// 		zippedArray.push(obj);
-// 	}
-
-// 	return zippedArray;
-// }
-
-// Rewrite the above function in typescript
-// Path: src/helpers/zip.ts
 export default function zip(keys: string[] = [], ...arrays: string[][]) {
 	const length = arrays[0]?.length;
 	if (!length) return undefined;
