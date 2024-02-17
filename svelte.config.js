9c36d9abeaaf659b27bffbce02b099a7aedf934e
Nick Trombley <trombley.nick@gmail.com>
2024-02-17 09:22:47 -0500

    Update dependencies and try deploying to netlify for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
+//import adapterNode from '@sveltejs/adapter-node';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapterNetlify(),
 		alias: {
 			$components: 'src/components',
 			$helpers: 'src/helpers',
