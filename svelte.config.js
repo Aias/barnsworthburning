6936a97b6aca9f589dd8545e8129bba094d5fb54
Nick Trombley <trombley.nick@gmail.com>
2024-02-17 09:27:27 -0500

    Add netlify stuff

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapterNetlify(),
+		adapter: adapterNetlify({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		}),
 		alias: {
 			$components: 'src/components',
 			$helpers: 'src/helpers',
