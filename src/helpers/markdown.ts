5d2b4816dd3483fc79e7e0d122da9e5aa3829441
Nick Trombley <trombley.nick@gmail.com>
2024-06-05 14:43:29 -0400

    Open links in new tab

--- src/helpers/markdown.ts
+++ src/helpers/markdown.ts
@@ -1,7 +1,19 @@
 import { Marked } from 'marked';
 
+const linkRenderer = (href: string, title: string | null | undefined, text: string) => {
+	const target = '_blank';
+	const titleTag = title ? ` title="${title}"` : '';
+	return `<a href="${href}" target="${target}"${titleTag}>${text}</a>`;
+};
+
+const renderer = {
+	link: linkRenderer
+};
+
 const markdown = new Marked({
 	breaks: true
 });
+
+markdown.use({ renderer });
 
 export default markdown;
