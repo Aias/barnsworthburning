6f6c03bb87e309c4e9f800a2f23174a495651cae
Nick Trombley <trombley.nick@gmail.com>
2024-06-05 08:47:41 -0400

    More robust handling for block links

--- src/components/BlockLink.svelte
+++ src/components/BlockLink.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import type { Snippet } from 'svelte';
 
 	interface BlockLinkProps {
@@ -14,20 +15,25 @@
 		// Adapted from:
 		// https://css-tricks.com/block-links-the-search-for-a-perfect-solution/#comment-1769257
 		// https://codepen.io/xelium/pen/OJbEVmL
+		const blockClass = '.block-link';
+		const linkSelector = `a, button, .link, :any-link`;
 		const target = event.target as HTMLElement;
-		let card: HTMLElement | null = null;
-		let mainLink: HTMLElement | null = null;
-		let allLinks: NodeListOf<HTMLElement> | null = null;
-		if (target) {
-			card = target.closest('.block-link');
-		}
-		if (card) {
-			mainLink = card.querySelector('.main-link');
-			allLinks = card.querySelectorAll('a, button, .link, :any-link');
-		}
-		if (!mainLink || window.getSelection()?.toString()) return;
-		if (allLinks && Array.from(allLinks).some((item) => item === target)) return;
-		const href = mainLink.getAttribute('href') ?? '/';
+		if (!target) return; // If we don't have a click target (somehow), no special handling.
+
+		const container = target.closest(blockClass);
+		const nearestLink = target.closest(`${linkSelector}, ${blockClass}`);
+		if (!container || !container.isSameNode(nearestLink)) return; // If we clicked within a link that's not the container, no special handling.
+
+		const mainLink = container.querySelector('.main-link');
+		const allLinks = container.querySelectorAll(linkSelector);
+		if (!mainLink || window.getSelection()?.toString()) return; // If the user has selected text, don't navigate.
+		if (allLinks && Array.from(allLinks).some((item) => item === target)) return; // If the click target is a link, no special handling.
+
+		const href = mainLink.getAttribute('href');
+		const { url } = $page;
+		if (!href || href === url.href) return; // If the main link doesn't have an href, or we're already on the page, no special handling.
+
+		// No other special cases apply, treat the click as a navigation to the main link's href.
 		event.preventDefault();
 		event.stopPropagation();
 		goto(href);
@@ -45,13 +51,12 @@
 </svelte:element>
 
 <style>
+	.block-link {
+		cursor: pointer;
+	}
 	.block-link :global {
 		&:has(.main-link:focus-visible) {
 			outline: 1px solid var(--main);
-		}
-		.main-link:focus-visible,
-		&:hover .main-link {
-			text-decoration: underline;
 		}
 	}
 </style>
